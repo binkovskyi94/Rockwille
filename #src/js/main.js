97a5084371ee55a053d5539f948f6d563404144a
binkovskyi94 <guidekinoguidekino@gmail.com>
2020-08-13 14:55:51 +0300

    added a feedback form and small animation button

--- #src/js/main.js
+++ #src/js/main.js
@@ -1,3 +1,15 @@
 $(function(){
-
+    $('.open__fast').click(function(){
+        $('.popup__fast').css({'top': $(window).scrollTop() +100}).addClass('active');
+        $('.bg__popup').fadeIn();
+    
+        $('.bg__popup').click(function(){
+            $('.popup__fast').removeClass('active');
+            $('.bg__popup').fadeOut();
+        });
+    });
 });
+
+$(window).scroll(function(){
+    $('.popup__fast').css({'top': $(window).scrollTop() +100})
+}).scroll();
